f458ab0c8bc0d687c612efe8d684cb98f4af417c
Nariman Huseynov <hnariman@gmail.com>
2020-05-30 23:18:56 +0400

    added some navbar just ot show the place of it

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import React from "react";
 import Carousel from "./components/Carousel";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
 export default class App extends React.Component {
   state = {};
   render() {
     return (
       <React.Fragment>
         <Header />
+        <div className="navbar">
+          <span> ANA SƏHİFƏ </span>
+          <span> MUZEY </span>
+          <span> TƏHSİL </span>
+          <span> EKSPOZİSİYA </span>
+          <span> KOLLEKSİYA </span>
+          <span> SƏRGİLƏR VƏ XƏBƏRLƏR </span>
+          <span> XİDMƏTLƏR </span>
+          <span> MAĞAZA </span>
+        </div>
         <Carousel />
+        {/* <Gifts /> */}
+        {/* <CarpetsAroundTheWorld /> */}
+        {/* <Supporters /> */}
+        {/* <CompulsoryGovtSites /> */}
         <Footer />
       </React.Fragment>
     );
