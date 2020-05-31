e83b014917c28aba2b843c3aff88045f8a2e49a5
Nariman Huseynov <hnariman@gmail.com>
2020-05-31 13:21:24 +0400

    Updates

---
+++ precache-manifest.fe308c550ffe0c607d004a40a94441cb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686f8a133eec713f8aa6db40e6811a90",
+    "url": "/carpets/index.html"
+  },
+  {
+    "revision": "4b154b614739e5db56dc",
+    "url": "/carpets/static/css/2.afd40c00.chunk.css"
+  },
+  {
+    "revision": "df4251bfbd8c11faad53",
+    "url": "/carpets/static/css/main.a62bcd54.chunk.css"
+  },
+  {
+    "revision": "4b154b614739e5db56dc",
+    "url": "/carpets/static/js/2.3481e6cd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/carpets/static/js/2.3481e6cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df4251bfbd8c11faad53",
+    "url": "/carpets/static/js/main.ee46fdcc.chunk.js"
+  },
+  {
+    "revision": "a4f0e77b7ae9b043d6a6",
+    "url": "/carpets/static/js/runtime-main.f87e1a80.js"
+  },
+  {
+    "revision": "ef50b637568e5d7af4f486f3e78d71f1",
+    "url": "/carpets/static/media/_ASA8309.ef50b637.jpg"
+  },
+  {
+    "revision": "923b389b8f979f417a27be051a2e2975",
+    "url": "/carpets/static/media/_ASA8365.923b389b.jpg"
+  },
+  {
+    "revision": "b41176d26a15f978912341d16896905c",
+    "url": "/carpets/static/media/_ASA8463.b41176d2.jpg"
+  }
+]);
