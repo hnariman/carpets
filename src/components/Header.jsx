46f2658a2973a1e194f319aa0078c9efdcf210b4
Nariman Huseynov <hnariman@gmail.com>
2020-06-02 03:09:02 +0400

    made separate unified Social links component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,20 @@
 import React from "react";
 import "./style.css";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Social } from "./Social";
+import { LanguageBar } from "./LanguageBar.jsx";
 
 library.add(fab, fas);
 export default class Header extends React.Component {
   state = {};
   render() {
     return (
-      <div className="header">
-        <div className="header-left">
-          <FontAwesomeIcon icon={faHome} />
-        </div>
-        <div className="header-center"></div>
-        <div className="header-right">
-          <FontAwesomeIcon icon={["fab", "facebook-f"]} />
-          <FontAwesomeIcon icon={["fab", "twitter"]} />
-          <FontAwesomeIcon icon={["fab", "instagram"]} />
-          <FontAwesomeIcon icon={["fab", "youtube"]} />
-          <div>
-            <FontAwesomeIcon icon={["fas", "language"]} />
-            <select id="" name="" className="languages">
-              <option value="">Azerbaijan</option>
-              <option value="">Russian</option>
-              <option value="">English</option>
-            </select>
-          </div>
-        </div>
-      </div>
+      <section className="header">
+        <Social />
+        <LanguageBar />
+      </section>
     );
   }
 }
