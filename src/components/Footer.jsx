c24eb353d59109ad5d5ab2efd718ca923b010a93
Nariman Huseynov <hnariman@gmail.com>
2020-06-01 15:09:01 +0400

    decomposed footer for the sake to be clear enough for future programmers

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,90 @@
 import React from "react";
+import "./style.css";
+import fasad from "../img/_ASA8463.jpg";
 
 export default class Footer extends React.Component {
   state = {};
   render() {
-    return <div>Footer</div>;
+    return (
+      <section className="footer">
+        <div className="footer-top">
+          <GovLinks />
+          <div className="logo">LOGO</div>
+        </div>
+        <hr />
+        <div className="footer-bottom">
+          <SocialLinks />
+        </div>
+      </section>
+    );
   }
 }
+
+const GovLinks = () => (
+  <ul className="footer-gov-links">
+    <li>
+      <a href="">
+        Link 1
+        <img src="" alt=" Link 1" />
+      </a>
+    </li>
+    <li> | </li>
+    <li>
+      <a href="">
+        Link 2
+        <img src="" alt="" />
+      </a>
+    </li>
+    <li> | </li>
+    <li>
+      <a href="">
+        Link 3
+        <img src="" alt="" />
+      </a>
+    </li>
+    <li> | </li>
+    <li>
+      <a href="">
+        Link 4
+        <img src="" alt="" />
+      </a>
+    </li>
+    <li> | </li>
+    <li>
+      <a href="">
+        Link 5
+        <img src="" alt="" />
+      </a>
+    </li>
+  </ul>
+);
+
+const SocialLinks = () => (
+  <ul className="footer-social-links">
+    <li>
+      <a href="">
+        <span>social link</span>
+      </a>
+    </li>
+    <li>
+      <a href="">
+        <span>social link</span>
+      </a>
+    </li>
+    <li>
+      <a href="">
+        <span>social link</span>
+      </a>
+    </li>
+    <li>
+      <a href="">
+        <span>social link</span>
+      </a>
+    </li>
+    <li>
+      <a href="">
+        <span>social link</span>
+      </a>
+    </li>
+  </ul>
+);
