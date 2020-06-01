8378844346c9924868211c3ca10a81634aa24b39
Nariman Huseynov <hnariman@gmail.com>
2020-06-02 03:09:41 +0400

    navbar links fixed and styled properly (only Link to be fiex and ready)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,23 @@
 import React from "react";
+import logo from "../img/logo.png";
 
 export default class Navbar extends React.Component {
   state = {};
   render() {
     return (
-      <div className="navbar">
-        <span>
+      <section>
+        <div className="navbar">
+          <img src={logo} alt="logo" />
           <a href="#"> ANA SƏHİFƏ </a>
-        </span>
-        <span>
           <a href="#"> MUZEY </a>
-        </span>
-        <span>
           <a href="#"> TƏHSİL </a>
-        </span>
-        <span>
           <a href="#"> EKSPOZİSİYA </a>
-        </span>
-        <span>
           <a href="#"> KOLLEKSİYA </a>
-        </span>
-        <span>
           <a href="#"> SƏRGİLƏR VƏ XƏBƏRLƏR </a>
-        </span>
-        <span>
           <a href="#"> XİDMƏTLƏR </a>
-        </span>
-        <span>
           <a href="#"> MAĞAZA </a>
-        </span>
-      </div>
+        </div>
+      </section>
     );
   }
 }
